fce9f36f0c13f64fba5e54028238ec14bf36fdef
karasidnez <karasdnez@gmail.com>
2026-04-08 14:23:28 +0300

    Push

--- bin/todo_app_v2.dart
+++ bin/todo_app_v2.dart
@@ -1,3 +1,14 @@
 void main(){
    print('Hello world!');
+   String name = 'Danara';
+   String? name2 = null;
+   var count = 0;
+   var title = 'hello';
+   // var x = 'dsf';
+   final id = 1;
+   const app = 'Todo';
+   print('$app $id ${count + 5}');
+   List<String> test = ['язык', 'уроки'];
+   Map<String, dynamic> data = {'key': 13};
+   Set<int> id2 = {1, 2, 3, 1};
 }
